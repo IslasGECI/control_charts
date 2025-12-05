57aa33b6dd50d4e9672fa443c1f85fef8c7a08bc
Guillermo Olvera <guillermo.olvera@islas.org.mx>
2025-12-05 11:57:10 -0800

    🧪 Add minimal examples

--- tests/test_transformations.py
+++ tests/test_transformations.py
@@ -1,4 +1,7 @@
 import control_charts as dt
+import pyshewhart
+import matplotlib.pyplot as plt
+import pandas as pd
 
 
 def test_add_offset():
@@ -7,3 +10,18 @@ def test_add_offset():
     expected = augend + addend
     obtained = dt.add_offset(augend, addend)
     assert expected == obtained
+
+
+measurements = pd.read_csv("tests/data/Lambda_W.csv")
+
+
+def test_example():
+    pyshewhart.XbarR(measurements["fecha"], measurements["Lambda"],
+                     sample_size=4)
+    plt.savefig("control_chart_lambda.png")
+    plt.close()
+
+    pyshewhart.XbarR(measurements["fecha"], measurements["W"],
+                     sample_size=4, check_western_elec_rules=False)
+    plt.savefig("control_chart_w.png")
+    plt.close()
